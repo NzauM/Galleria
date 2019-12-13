7c43c0be3eabf5d4ee37f16b3a90741aa5cdf98a
NzauM <mercywaenu16@gmail.com>
2019-12-13 15:37:41 +0300

    Add the media settings

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Picture
+
 
 # Create your views here.
+def pics(request):
+    pics = Picture.all_pics()
+    return render(request,'all_pics.html',{"pics":pics})
