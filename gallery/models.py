c336dc0ac1860525ca14b4dfca524ae708320587
NzauM <mercywaenu16@gmail.com>
2019-12-13 13:06:28 +0300

    Add a picture model clas and its foreign key classes

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Pictures(models.Model):
-    image = models.ImageField(upload_to='')
+class Author(models.Model):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length= 50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=300,blank=True)
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=80)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=80)
+
+class Picture(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    location = models.ForeignKey(Location,on_delete = models.CASCADE,)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE,)
+    author = models.ForeignKey(Author,on_delete = models.CASCADE)
