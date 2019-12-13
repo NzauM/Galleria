9104f0dfd6f6973c74aa64c6135765e2900a75cc
NzauM <mercywaenu16@gmail.com>
2019-12-13 12:06:37 +0300

    Add the db to galleria settings

--- Galleria/settings.py
+++ Galleria/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'Galleria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER':'moringa',
+        'PASSWORD':'Moringa'
     }
 }
 
@@ -105,8 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
 
 USE_L10N = True
